901e3047aab1be81c4e3e56a7e1a94e57518e50d
Al Sutton <al.sutton@alsutton.com>
2021-02-25 11:49:16 +0000

    Add Sharded JUnit5 Example

---
+++ examples/junit5/build.gradle.kts
@@ -0,0 +1,23 @@
+import net.ltgt.gradle.errorprone.errorprone
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+val junitVersion="5.7.0"
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+    testImplementation(project(":test-framework-bridges:junit5"))
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ examples/junit5/src/test/java/com/alsutton/shardedtestexecutor/example/DummyTestClass3.java
@@ -0,0 +1,15 @@
+package com.alsutton.shardedtestexecutor.example;
+
+import com.alsutton.shardedtestexecutor.junit5.ShardedTestExecutorExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith({ShardedTestExecutorExtension.class})
+public class DummyTestClass3 {
+    @Test
+    public void dummyTestMethod() {
+        assertEquals(1 , 1);
+    }
+}
