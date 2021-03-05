1b203db8bc7f93bce22f0e7c78353b5d1ae1b08f
Al Sutton <al.sutton@alsutton.com>
2021-03-05 08:07:51 +0000

    Tidy up imports

--- test-framework-bridges/junit4/src/main/java/com/alsutton/shardedtestexecutor/junit4/ShardedTestExecutorRunner.java
+++ test-framework-bridges/junit4/src/main/java/com/alsutton/shardedtestexecutor/junit4/ShardedTestExecutorRunner.java
@@ -1,16 +1,9 @@
 package com.alsutton.shardedtestexecutor.junit4;
 
 import com.alsutton.shardedtestexecutor.TestExecutionPredicate;
-import org.junit.internal.runners.JUnit4ClassRunner;
-import org.junit.runner.Description;
-import org.junit.runner.FilterFactory;
-import org.junit.runner.FilterFactoryParams;
-import org.junit.runner.Runner;
-import org.junit.runner.manipulation.Filter;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
-import org.junit.runners.model.TestClass;
 
 public class ShardedTestExecutorRunner extends BlockJUnit4ClassRunner {
     private final TestExecutionPredicate testExecutionPredicate = new TestExecutionPredicate();
